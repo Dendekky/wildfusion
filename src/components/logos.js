f2539dab115ec470d3bd52aa393f41bb3fc198c7
Ibrahim <adeniyiib360@gmail.com>
2020-08-22 19:49:19 +0100

    feat: create 2 out of five sections

---
+++ src/components/logos.js
@@ -0,0 +1,19 @@
+import Headerlogo from '../assets/logos/logo.svg';
+import Culture from '../assets/logos/icon__goals-culture.svg';
+import Efficiencies from '../assets/logos/icon__goals-efficiencies.svg';
+import ElasticScaling from '../assets/logos/icon__goals-elastic-scaling.svg';
+import Foundation from '../assets/logos/icon__goals-foundation.svg';
+import Innovation from '../assets/logos/icon__goals-innovation.svg';
+import Speed from '../assets/logos/icon__goals-speed.svg';
+import Transformation from '../assets/logos/icon__goals-transformation.svg';
+
+export {
+    Headerlogo,
+    Culture,
+    Efficiencies,
+    ElasticScaling,
+    Foundation,
+    Innovation,
+    Speed,
+    Transformation
+}
